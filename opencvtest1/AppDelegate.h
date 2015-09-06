a4e224b7316cbd7914a8dc7d8fefb7b5fe8aa670
Yanliang Gu <Leo@rover-226-70.rovernet.mtu.edu>
2015-09-06 18:27:25 -0400

    Initial Commit

---
+++ opencvtest1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  opencvtest1
+//
+//  Created by Yanliang Gu on 9/6/15.
+//  Copyright (c) 2015 Yanliang Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ opencvtest1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  opencvtest1
+//
+//  Created by Yanliang Gu on 9/6/15.
+//  Copyright (c) 2015 Yanliang Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
